c0c814d5037fd48574e6b6d7319f31222d410d0b
Leph <leph.quentinrouxel@gmail.com>
2019-02-27 14:18:51 +0000

    Fix multithread bug in the LockFreeDoubleQueue

--- Common/include/rhio_common/LockFreeDoubleQueue.hpp
+++ Common/include/rhio_common/LockFreeDoubleQueue.hpp
@@ -27,17 +27,15 @@ class LockFreeDoubleQueue
          */
         LockFreeDoubleQueue(size_t maxSize) :
             _maxSize(maxSize),
-            _isWritingTo1(1),
-            _stillWriting1(0),
-            _stillWriting2(0),
-            _length1(0),
-            _length2(0),
-            _buffer1(),
-            _buffer2()
+            _state1(true),
+            _state2(false),
+            _statePtr(nullptr)
         {
+            //State initialization
+            _statePtr.store(&_state1);
             //Memory pre-allocation
-            _buffer1.resize(_maxSize);
-            _buffer2.resize(_maxSize);
+            _state1.buffer.resize(_maxSize);
+            _state2.buffer.resize(_maxSize);
         }
 
         /**
@@ -46,6 +44,27 @@ class LockFreeDoubleQueue
          */
         void swapBufferFromReader()
         {
+            //Retrieve writing buffer
+            State_t* stateOld = _statePtr.load();
+            //Reset reading buffer size
+            if (stateOld->isWritingTo1) {
+                _state2.length.store(0);
+            } else {
+                _state1.length.store(0);
+            }
+            //Atomically inverse the flag
+            if (stateOld->isWritingTo1) {
+                _statePtr.store(&_state2);
+            } else {
+                _statePtr.store(&_state1);
+            }
+            //Actively wait that all writer complete 
+            //writing to old writing buffer
+            while (stateOld->stillWriting.load() != 0) {
+                //Wait
+            }
+
+            /*
             //Retrieve writing buffer
             int isOldWriting1 = _isWritingTo1.load();
             //Reset reading buffer size
@@ -67,6 +86,7 @@ class LockFreeDoubleQueue
                     //Wait
                 }
             }
+            */
         }
 
         /**
@@ -74,10 +94,10 @@ class LockFreeDoubleQueue
          */
         const std::vector<T>& getBufferFromReader() const
         {
-            if (_isWritingTo1.load()) {
-                return _buffer2;
+            if (_statePtr.load()->isWritingTo1) {
+                return _state2.buffer;
             } else {
-                return _buffer1;
+                return _state1.buffer;
             }
         }
 
@@ -87,14 +107,14 @@ class LockFreeDoubleQueue
         size_t getSizeFromReader() const
         {
             size_t length;
-            if (_isWritingTo1.load()) {
-                length = _length2.load();
+            if (_statePtr.load()->isWritingTo1) {
+                length = _state2.length.load();
             } else {
-                length = _length1.load();
+                length = _state1.length.load();
             }
             
             //Shrink actual size to maximum size since writers 
-            //increase length before checking for for length limit
+            //increase length before checking for length limit
             if (length > _maxSize) {
                 length = _maxSize;
             }
@@ -110,33 +130,20 @@ class LockFreeDoubleQueue
         bool appendFromWriter(const T& val)
         {
             bool isOk = false;
-            if (_isWritingTo1.load()) {
-                //Take writing lock on buffer (wait in swapBuffer)
-                _stillWriting1.fetch_add(1);
-                //Retrieve next to write index and
-                //atomically increase it (after reading)
-                size_t index = _length1.fetch_add(1);
-                //Write to buffer of not full
-                if (index < _maxSize) {
-                    _buffer1[index] = val;
-                    isOk = true;
-                }
-                //Release the lock
-                _stillWriting1.fetch_sub(1);
-            } else {
-                //Take writing lock on buffer (wait in swapBuffer)
-                _stillWriting2.fetch_add(1);
-                //Retrieve next to write index and
-                //atomically increase it (after reading)
-                size_t index = _length2.fetch_add(1);
-                //Write to buffer of not full
-                if (index < _maxSize) {
-                    _buffer2[index] = val;
-                    isOk = true;
-                }
-                //Release the lock
-                _stillWriting2.fetch_sub(1);
+            //Retrieve current writting buffer state
+            State_t* state = _statePtr.load();
+            //Take writing lock on buffer (wait in swapBuffer)
+            state->stillWriting.fetch_add(1);
+            //Retrieve next to write index and
+            //atomically increase it (after reading)
+            size_t index = state->length.fetch_add(1);
+            //Write to buffer of not full
+            if (index < _maxSize) {
+                state->buffer[index] = val;
+                isOk = true;
             }
+            //Release the lock
+            state->stillWriting.fetch_sub(1);
 
             return isOk;
         }
@@ -144,37 +151,59 @@ class LockFreeDoubleQueue
     private:
 
         /**
+         * Structure for buffer state
+         */
+        struct State_t {
+            /**
+             * If true, the buffer 1 is 
+             * currently used to write on it.
+             * Else, buffer 2 is used.
+             */
+            const bool isWritingTo1;
+            /**
+             * Hold the number of writers
+             * currently writing to the buffer
+             */
+            std::atomic<int> stillWriting;
+            /**
+             * Index of the next available 
+             * writable cell in the buffer
+             */
+            std::atomic<size_t> length;
+            /**
+             * Internal pre-allocated buffers
+             */
+            std::vector<T> buffer;
+
+            /**
+             * Initialization
+             */
+            State_t(bool isWritting) :
+                isWritingTo1(isWritting),
+                stillWriting(0),
+                length(0),
+                buffer()
+            {
+            }
+        };
+        
+        /**
          * Maximum data contained by each buffers.
          * Use for pre-dynamic allocation.
          */
         const size_t _maxSize;
 
         /**
-         * If non zero, the writers must write 
-         * into internal buffer 1. 
-         * If zero, the buffer 2 is used.
+         * State for double buffer 1 and 2
          */
-        std::atomic<int> _isWritingTo1;
-        
-        /**
-         * Hold the number of writers
-         * currently writing to buffer 1 and 2
-         */
-        std::atomic<int> _stillWriting1;
-        std::atomic<int> _stillWriting2;
+        State_t _state1;
+        State_t _state2;
 
         /**
-         * Index of next available writable
-         * in buffer 1 and 2.
+         * Pointer toward the state of the
+         * buffer currently used for writting
          */
-        std::atomic<size_t> _length1;
-        std::atomic<size_t> _length2;
-
-        /**
-         * Internal pre-allocated buffers
-         */
-        std::vector<T> _buffer1;
-        std::vector<T> _buffer2;
+        std::atomic<State_t*> _statePtr;
 };
 
 }
